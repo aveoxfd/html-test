ae4d65018aeae672709e00bb24018a620bd88440
.ffd <krekerbruh@hotmail.com>
2026-02-12 09:13:20 +0300

    v1.0

---
+++ js/jq.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("div").bind("mouseover", function(){
+        $(this).css({"background-color":"#ffeaea6e", cursor: "pointer"});   
+    }).bind("mouseout", function(){
+        $(this).css({"background-color":"",cursor: ""});
+    });
+    $("#something").animate({
+        'margin-top': '300px', 'margin-left': '400px'
+    }, 1000); //all elements that have id something
+
+    $('.something') // all element that have class something
+
+    $('div p #something a') //takes all refers in paragraphs that have id 'something' and have parent div
+
+    $('.code p').click(function(){ // all p that have class code
+        if ($(this).hasClass('a')){
+            alert('this paragraph has class a')
+        }
+        else (alert('we have not found this'))
+    })
+
+});
